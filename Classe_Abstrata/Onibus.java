be5fab6ca71507c4aa019fa02b68d2f56a7a51d9
LemuelFreitas <132360202+LemuelFreitas@users.noreply.github.com>
2023-11-07 13:37:10 -0300

    Add files via upload

---
+++ Classe_Abstrata/Onibus.java
@@ -0,0 +1,34 @@
+package Classe_Abstrata;
+
+public class Onibus extends veiculos{
+
+	public Onibus(String placa, String modelo, String cor, int ano) {
+		super(placa, modelo, cor, ano);
+		
+	}
+
+	@Override
+	public void acelerar() {
+		System.out.println("O onibus está acelerando");
+		
+	}
+
+	@Override
+	public void frear() {
+		System.out.println("O onibus está freando");
+		
+	}
+
+	@Override
+	public void virar() {
+		System.out.println("O onibus está virando");
+		
+	}
+
+	@Override
+	public void ligar() {
+		System.out.println("O onibus está ligado");
+		
+	}
+	
+}
